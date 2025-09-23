75e7dee9c7d0442714048bff1d3769234dd3ec1c
Felipe Del Campo <fdelcampo@pragma.cl>
2025-09-22 22:14:14 -0300

    dataAccess

--- CreaBD_TablaUsuario.sql
+++ CreaBD_TablaUsuario.sql
@@ -1,15 +1,13 @@
 
-Create database UsuariosDB
-
-use UsuariosDB
+use Usuarios
 go
 drop table usuarios
 
 CREATE TABLE [dbo].[Usuarios](
 	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[Nombre] [nvarchar](50) NOT NULL,
+	[Nombre] [nvarchar](255) NOT NULL,
 	[Rut] [nvarchar](50) unique NOT NULL,
-	[Correo] [nvarchar](200) NULL,
+	[Correo] [nvarchar](255) NULL,
 	[FechaNacimiento] [date] NOT NULL,
  CONSTRAINT [PK_Usuarios] PRIMARY KEY CLUSTERED 
 (
